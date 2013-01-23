e387a14249b62cc7f1307eb56c12b7f97ba9d6ee
Weisi Dai <multiple1902@gmail.com>
2013-01-24 05:44:55 +0800

    integration with bootstrap-multiselect

--- repartee/forms.py
+++ repartee/forms.py
@@ -2,13 +2,13 @@ from django import forms
 from models import App, Behavior, OS, Protocol
 
 class QueryForm(forms.Form):
-    beginTime   = forms.DateTimeField(required = False, widget = forms.TextInput(attrs = {'class': 'dateTimeInput'}), label = u"Begin Time")
-    endTime     = forms.DateTimeField(required = False, widget = forms.TextInput(attrs = {'class': 'dateTimeInput'}), label = "End Time")
+    beginTime   = forms.DateTimeField(required = False, widget = forms.TextInput(attrs = {'class': 'dateTimeInput'}), label = u"BeginTime between")
+    endTime     = forms.DateTimeField(required = False, widget = forms.TextInput(attrs = {'class': 'dateTimeInput'}), label = "and")
     #app         = forms.ModelChoiceField(queryset = App.objects.all(), required = False)
-    behavior    = forms.ModelMultipleChoiceField(queryset = Behavior.objects.all(), required = False, label = "Behavior(s)")
-    os          = forms.ModelMultipleChoiceField(queryset = OS.objects.all(), required = False, label = "OS(s)")
+    behavior    = forms.ModelMultipleChoiceField(queryset = Behavior.objects.all(), widget = forms.SelectMultiple(attrs = {'class': 'multiselect-behavior'}), required = False, label = "Behavior(s)")
+    os          = forms.ModelMultipleChoiceField(queryset = OS.objects.all(), widget = forms.SelectMultiple(attrs = {'class': 'multiselect'}), required = False, label = "OS(s)")
     srcIp       = forms.CharField(required = False, label = "Source IP")
     srcPort     = forms.IntegerField(required = False, label = "Source Port")
     dstIp       = forms.CharField(required = False, label = "Destination IP")
     dstPort     = forms.IntegerField(required = False, label = "Destination Port")
-    protocol    = forms.ModelMultipleChoiceField(queryset = Protocol.objects.all(), required = False, label = "Protocol(s)")
+    protocol    = forms.ModelMultipleChoiceField(queryset = Protocol.objects.all(), widget = forms.SelectMultiple(attrs = {'class': 'multiselect'}), required = False, label = "Protocol(s)")
--- repartee/query.py
+++ repartee/query.py
@@ -1,17 +1,32 @@
 from django.shortcuts import render_to_response
 from django.template import loader,Context, RequestContext
+from django.db.models import Q
+import md5
 
-from models import App, Behavior, OS, Protocol
+from models import Flow, App, Behavior, OS, Protocol
 from forms import QueryForm
 
 def queryHome(request):
     if request.method == "POST":
         # display query.query.html which includes queryResult.inc.html
         queryForm = QueryForm(request.POST)
+
         print queryForm.is_valid()
+        queryID = md5.new(str(queryForm.cleaned_data)).hexdigest()
+
+        if "queries" not in request.session:
+            request.session["queries"] = {}
+
+        request.session["queries"][queryID] = queryForm
+
+        flows = Flow.objects.filter(getQFromQueryForm(queryForm))
+
         return render_to_response("query.query.html", {
                 'queryForm': queryForm,
+                'queryID': queryID,
+                'resultsCount': len(flows),
             }, context_instance = RequestContext(request))
+
     else:
         # display query.home.html
         queryForm = QueryForm()
@@ -19,3 +34,11 @@ def queryHome(request):
                 'queryForm': queryForm,
             }, context_instance = RequestContext(request))
 
+def getQFromQueryForm(queryForm):
+    q = Q()
+
+    if queryForm.cleaned_data["beginTime"] and queryForm.cleaned_data["endTime"]:
+        q = q | Q(beginTime__gte = queryForm.cleaned_data["beginTime"],
+                  beginTime__lte = queryForm.cleaned_data["endTime"])
+
+    return q
